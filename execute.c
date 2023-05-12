e4aa67a3fc7a53108959b58fb65672b874869508
MahmoudEasa <mu01011422865@gmail.com>
2023-05-12 18:46:44 +0300

    executes the command ls -l /tmp in 5 different child processes

--- execute.c
+++ execute.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/type.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+
+void exe(char **argv);
 
 /**
  * main - executes the command ls -l /tmp in 5 different child processes
@@ -11,10 +13,40 @@
 
 int main(void)
 {
-	pid_t child;
+	pid_t child, child_pid, parent;
+	int status, i;
+	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
+
+	(void)i;
+	for (i = 0; i < 5; i++)
+	{
+		child = fork();
+		if (child == -1)
+		{
+			perror("Error");
+			return (1);
+		}
+
+		if (child == 0)
+		{
+			child_pid = getpid();
+			parent = getppid();
+			printf("\n--- child PID: %u parent: %u ---\n", child_pid, parent);
+			exe(argv);
+		}
+		else
+			wait(&status);
+	}
+
+	return (0);
+}
+
+void exe(char **argv)
+{
 	int status;
-	char *argv[] = {"/bin/ls", "-l", "/tmp"};
 
-
+	status = execve(argv[0], argv, NULL);
+	if (status == -1)
+		perror("Error");
 }
 
